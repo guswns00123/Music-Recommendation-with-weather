f5c32ded040171c88e7720ece65b8199e87c2ea6
yoo <fresh0911@naver.com>
2024-01-03 15:42:41 +0900

    weather

---
+++ dags/dags_seoul_api_corona.py
@@ -0,0 +1,21 @@
+from operators.seoul_api_to_csv_operator import SeoulApiToCsvOperator
+from airflow import DAG
+import pendulum
+
+with DAG(
+    dag_id='dags_seoul_api_corona',
+    schedule='0 7 * * *',
+    start_date=pendulum.datetime(2024,1,2, tz='Asia/Seoul'),
+    catchup=False
+) as dag:
+    
+    tb_corona19_count_status = SeoulApiToCsvOperator(
+        task_id='tb_corona19_count_status',
+        dataset_nm='TbCorona19CountStatus',
+        path='/opt/airflow/files/TbCorona19CountStatus/{{data_interval_end.in_timezone("Asia/Seoul") | ds_nodash }}',
+        file_name='TbCorona19CountStatus.csv'
+    )
+    
+    
+
+    tb_corona19_count_status
